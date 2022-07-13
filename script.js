e676d381dd9bc6929a0ce9d04e43b06ed08953dc
DristiGurung <dristigurung1@gmail.com>
2022-07-13 09:19:24 +0545

    added event listener and timer function

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
+
+// 1. select all tabs from tab list.
+const pomodoroTab = document.getElementById("pomodoro");
+const shortBreakTab = document.getElementById("shortBreak");
+const longBreakTab = document.getElementById("longBreak");
+
+const pomodoroContentTab = document.getElementById("pomodoro-content");
+const shortBreakContentTab = document.getElementById("shortBreak-content");
+const longBreakContentTab = document.getElementById("longBreak-content");
+
+
+// 2. add click event listener to each tab.
+pomodoroTab.addEventListener("click",function(){
+
+    // 3. remove all the active tab class from tab list.
+    // 4. add active class to selected tab.
+
+    pomodoroTab.classList.add("active");
+    shortBreakTab.classList.remove("active");
+    longBreakTab.classList.remove("active");
+    
+    // 5. remove all activ class from tab content.
+    // 6. add active class to selected tab content.
+
+    pomodoroContentTab.classList.add("active")
+    shortBreakContentTab.classList.remove("active")
+    longBreakContentTab.classList.remove("active")
+});
+shortBreakTab.addEventListener("click",function(){
+    pomodoroTab.classList.remove("active");
+    shortBreakTab.classList.add("active");
+    longBreakTab.classList.remove("active");
+    
+
+    pomodoroContentTab.classList.remove("active")
+    shortBreakContentTab.classList.add("active")
+    longBreakContentTab.classList.remove("active")
+
+});
+longBreakTab.addEventListener("click",function(){
+    pomodoroTab.classList.remove("active");
+    shortBreakTab.classList.remove("active");
+    longBreakTab.classList.add("active");
+    
+
+    pomodoroContentTab.classList.remove("active")
+    shortBreakContentTab.classList.remove("active")
+    longBreakContentTab.classList.add("active")
+});
 // timer to develop
+function timer(seconds){
+ const now = Date.now();
+ const then = now + (seconds *1000);
+ const timerInterval =setInterval(function(){
+   const secondsLeft = Math.round((then - Date.now())/1000);
+   console.log(secondsLeft);
+
+   if(secondsLeft <= 0) {
+      clearInterval(timerInterval);
+      return 0;
+   }
+ },1000);
+
+};
+timer(5);
+
+
